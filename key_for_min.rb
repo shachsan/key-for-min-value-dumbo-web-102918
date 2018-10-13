e7e0ce738bf636cad1aba04b99c2b707afc30cef
SANJAY K SAI <shachsan@hotmail.com>
2018-10-13 03:31:58 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,10 @@
 
 def key_for_min_value(name_hash)
   min=0
-  p min
   name_hash.each do |k, v|
+    p v
     if v < min
-
-      p min = v
-
+      min = v
     end
   end
   return name_hash.key(min)
